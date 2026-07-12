06eba90101cac185269c263a89ce115e8504b2c6
musicoffeecol <musicoffeecol@gmail.com>
2026-07-12 07:57:38 -0500

    Create next.config.mjs
    feat: Configuration

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+// next.config.mjs
+
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/moc-web",
+  assetPrefix: "/moc-web/",
+};
+
+export default nextConfig;
